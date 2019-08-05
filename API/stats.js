3dd384d0c63dc6d738f1339039ed1dd3bd9fb369
Simon Hozjan <simonh@staninvest.si>
2019-08-05 10:03:51 +0200

    v1

--- API/stats.js
+++ API/stats.js
@@ -9,12 +9,15 @@ var config = {
   };
   firebase.initializeApp(config);
   var db = firebase.firestore();
+  var chart;
 
 var username="";
 getCookie("username");
 
 $(document).ready(function(){
 spremeniUsername();
+addBlockSites();
+addVisits();
 });
 
 function getCookie(c_name) {
@@ -29,4 +32,76 @@ function getCookie(c_name) {
      host.innerHTML = "Uporabnikško: "+username;
     }
   }
+
+  function addBlockSites(){
+    var db = firebase.firestore();
+    var docRef = db.collection("Users"); 
+
+    var blocks = document.getElementById("blocksSite");
+    if(blocks){
+      docRef.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            var user=doc.data();
+           
+            if(user.hostname==username){
+              st=user.blocks;    
+             
+              for(var i=0;i<st.length;i++){    
+                blocks.innerHTML += "<div class='okno'> <br>"+ st[i]+"     "+"<button class='button4'>odstrani</button> <br> </div>"+"<br>";                       
+              }
+            }
+        });
+    });
+    }
+  }
+
+  function addVisits(){
+    chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      title: {
+        text: "Število obiskov spletnih strani"
+      },
+      data: [{
+        type: "pie",
+        startAngle: 240,
+        yValueFormatString: "##0.00\"%\"",
+        indexLabel: "{label} {y}",         
+        dataPoints: [ 
+        ]
+      }]
+    });
+    chart.render();
+
+    var db = firebase.firestore();
+    var docRef = db.collection("Users"); 
+
+    var visit = document.getElementById("visits");
+    if(visit){
+      docRef.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            var user=doc.data();
+           
+            if(user.hostname==username){
+              st=user.blocks;    
+              arraySites=user.visitSites;
+              var steviloVisit=0;
+
+              for(var j=0;j<arraySites.length;j++) {
+               steviloVisit=steviloVisit+arraySites[j].visits;
+              }        
+
+                for(var j=0;j<arraySites.length;j++) {
+                  var procent=(arraySites[j].visits*100) /steviloVisit;
+                  chart.options.data[0].dataPoints.push({ y: procent, label: arraySites[j].site});
+                }            
+                
+                chart.render(); 
+                
+               
+              }  
+            });
+          });
+    }
+  }
+
 
